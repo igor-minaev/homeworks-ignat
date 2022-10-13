ad5c54c32e9124004a56e8d707d84d546c9776c5
Igor Minayeu <igor.minayeu@gmail.com>
2022-10-13 21:33:50 +0300

    done hw 2

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,23 +4,26 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType> // need to fix any
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+        props.setFilter("all")
     }
     const setHigh = () => {
+        props.setFilter("high")
         // need to fix
     }
     const setMiddle = () => {
+        props.setFilter("middle")
         // need to fix
     }
     const setLow = () => {
+        props.setFilter("low")
         // need to fix
     }
 
@@ -38,7 +41,7 @@ function Affairs(props: AffairsPropsType) {
     ))
 
     return (
-        <div>
+        <div className={s.container}>
             <div className={s.buttonContainer}>
                 <button
                     id={'hw2-button-all'}
